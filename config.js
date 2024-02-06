e8384b8d70a3bf6851d2eaf2e04c8caf0510470f
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-02-06 07:22:48 -0800

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ module.exports = {
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "Or_AN_YA_sonPr_AN_YA_aised", 
+  sessionId: process.env.SESSION_ID || "Nur_AN_YA_sesWr_AN_YA_estle", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
